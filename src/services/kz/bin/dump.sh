12bb5a5a0db6a050fea96a15d06b34a3759439ad
Ilya Ashchepkov <koctep@gmail.com>
2025-02-19 14:35:05 +0100

    Dump/init sysconfigs

--- src/services/kz/bin/dump.sh
+++ src/services/kz/bin/dump.sh
@@ -1,17 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 data_dir=${data_dir:-/usr/local/kazoo/docs}
+URL=${URL:-http://localhost:8000/v2}
+CREDS=${CREDS:-$(echo -n kazoo:kazoo | md5sum | awk '{print $1}')}
+ACCOUNT_NAME=${ACCOUNT_NAME:-kazoo}
 
 (
-CURL0="curl http://localhost:8000/v2"
-CREDS=$(echo -n kazoo:kazoo | md5sum | awk '{print $1}')
+CURL0="curl $URL/v2"
 AUTH_TOKEN=$($CURL0/user_auth \
   -X PUT \
-  -d"{\"data\":{\"account_name\":\"kazoo\",\"credentials\":\"$CREDS\"}}" \
+  -d"{\"data\":{\"account_name\":\"${ACCOUNT_NAME}\",\"credentials\":\"$CREDS\"}}" \
   | jq -r '.auth_token')
 ROOT_ACCOUNT_ID=$(jq -Rr 'split(".") | .[1] | @base64d | fromjson | .account_id' <<< $AUTH_TOKEN)
 
-mkdir -p $data_dir
+mkdir -p $data_dir/system_configs
+for conf in $($CURL0/system_configs?paginate=false -H "X-Auth-Token: $AUTH_TOKEN" | jq -r '.data[]'); do
+  echo dumping /system_configs/$conf
+  $CURL0/system_configs/$conf -H "X-Auth-Token: $AUTH_TOKEN"  | jq '{data: .data}' > $data_dir/system_configs/$conf.json
+done
 
 for acc in $($CURL0/accounts/${ROOT_ACCOUNT_ID}/descendants -H "X-Auth-Token: $AUTH_TOKEN" | jq -r '.data[].id'); do
   CURL=$CURL0/accounts/$acc
